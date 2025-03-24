9a6e9138ed5101368f76ad76f4398df4950da766
shushant <shushantkumar0625@gmail.com>
2025-03-25 02:14:35 +0530

    change

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api/",
-   withCredentials:true,
+    baseURL: import.meta.env.VITE_BACKEND_URL + "/api/",
+    withCredentials:true,
 });
